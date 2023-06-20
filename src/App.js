7e4db7c8a358bcb9b8db2fa5a529a283ae6405fc
Taiywo <54299107+Taiywo@users.noreply.github.com>
2023-06-20 12:54:40 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   // State variables to manage search query, recipes, and errors
   const [searchQuery, setSearchQuery] = useState('') // Search query input value
   const [recipes, setRecipes] = useState([])  // Array of recipes
-  const [error, setError] = useState(null) // Error message
+// Error message
 
 
   // Function to fetch recipes from the API
@@ -20,14 +20,11 @@ function App() {
     const response = await fetch(
       `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchQuery}`
     );
-    if (!response.ok) {
-      setError('Failed to fetch recipes');// Throw an error if the response is not successful
-      return;
-    }
+   
     const data = await response.json(); // Parsing the response as JSON
 
     setRecipes(data.meals);// Set the recipes state with the fetched data
-    setError(null);// Reset the error state
+
   };
 
 
@@ -50,7 +47,7 @@ function App() {
       <div className="flex flex-wrap gap-[2rem] gap-y-20 justify-evenly mx-auto my-0 pt-32 p-10">
          {/* Conditional rendering based on recipes state */}
         {!recipes ? (<div className="flex flex-col items-center"><img src={errorImg} alt="" className="w-[20rem]" />
-          <h1 className="text-3xl font-bold">Recipe Not Found</h1>{error}</div>)
+          <h1 className="text-3xl font-bold">Recipe Not Found</h1></div>)
           : (recipes.map(meals => 
            // Recipe card component
           <RecipeCard
