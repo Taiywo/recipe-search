0f861fa6db44987d0421dc55807068d82535b706
Taiywo <54299107+Taiywo@users.noreply.github.com>
2023-06-20 11:09:57 +0100

    Update SearchBar.jsx

--- src/component/SearchBar.jsx
+++ src/component/SearchBar.jsx
@@ -11,14 +11,17 @@ function SearchBar({
 }) {
   return (
     < div>
-      <header className='flex bg-primary  h-20 justify-center items-center shadow-lg'>
-        <img src={Logo} alt='' />
-        <h1 className='text-3xl font-serif text-white text-center'> CODENT  RECIPE</h1>
+      <header className='flex bg-primary h-20 justify-center flex-col w-screen items-center shadow-lg fixed z-10'>
+        <div className='flex bg-primary h-20 justify-center  gap-2 items-center'>
+          <img src={Logo} alt='' className='h-7' />
+          <h1 className='text-3xl font-serif text-white text-center'> CODENT  RECIPE</h1>
+        </div>
+        <h1 className='font-medium text-xs pb-3 text-white tracking-widest'>. . . . find Inspiring Recipes with Ease!</h1>
       </header >
-      <img className='bg-bgImage h-40 w-screen bg-cover bg-no-repeat bg-center'/>
+      <img className='bg-bgImage h-40 w-screen bg-cover bg-no-repeat bg-center' />
       <div className='flex justify-center '>
-        <div className='h-40 w-[60%] bg-primary  rounded-2xl border-white border-2 flex flex-col justify-around
-         absolute top-32 '>
+        <div className='h-24 w-[50%] bg-primary  rounded-2xl border-white border-2 flex flex-col justify-around
+         absolute top-32 z-1 '>
           <form onSubmit={handleSubmit}>
             <input
               value={query}
@@ -31,7 +34,7 @@ function SearchBar({
               type="submit"
               className="rounded-r-xl p-2 text-white cursor-pointer "
               value="Search"
-              
+
             />
           </form>
 
